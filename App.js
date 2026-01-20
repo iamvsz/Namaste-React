ca0c99100dff953f5efe5eea5c71cd88bcdac33d
Vishal Zade <vishalzade75@gmail.com>
2026-01-20 14:02:44 +0530

    adjacent siblings

--- App.js
+++ App.js
@@ -3,6 +3,7 @@
  * <div id="parent">
  *     <div id="child">
  *         <h1> I'm h1 tag</h1>
+ *         <h2> I'm h2 tag</h2>
  *     </div>
  * </div>
  *
@@ -13,11 +14,10 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
+  React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
-  ),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
 );
 
 console.log(parent); //object
