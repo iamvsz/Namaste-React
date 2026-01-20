4e7a6d6d0a6a990b1db39debc721b217eb2bf580
Vishal Zade <vishalzade75@gmail.com>
2026-01-20 13:03:47 +0530

    splitting app

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
-const heading = React.createElement("h1", {}, "Hello World from React!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc"},
+  "Hello World from React!",
+);
+
+console.log(heading); //object 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
